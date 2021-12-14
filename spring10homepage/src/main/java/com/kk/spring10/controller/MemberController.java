e7b4f758c01729864881109a7538575a0e1b9ec7
triptocode <3580rio@gmail.com>
2021-12-14 15:10:55 +0900

    게시판,items="${model키}",삼항연산자,(name키 value값->컨트롤러)

--- spring10homepage/src/main/java/com/kk/spring10/controller/MemberController.java
+++ spring10homepage/src/main/java/com/kk/spring10/controller/MemberController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,8 +17,12 @@ import com.kk.spring10.entity.MemberDto;
 @RequestMapping("/member")
 public class MemberController {
 	
+
 	@Autowired
 	private SqlSession sqlSession;
+	
+	@Autowired
+	private PasswordEncoder encoder;
 	
 	@GetMapping("/join")
 	public String join(){	
@@ -30,8 +35,11 @@ public String join(@ModelAttribute MemberDto memberDto) {
      MemberDto findId = 
 	 sqlSession.selectOne("mnMember.siFindId", memberDto.getMember_id()); 
 		
-     if(findId==null) {
-		   sqlSession.insert("mnMember.isJoin", memberDto);
+     if(findId==null) { // DB에 중복되는 id가 없으면 , 다음과 같이 pw 암호화 한다
+    	 String enc = encoder.encode(memberDto.getMember_pw()); // pw 암호화
+    	 memberDto.setMember_pw(enc); // 암호화된 pw 덮어쓰기 
+             
+		   sqlSession.insert("mnMember.iiJoin", memberDto);
 		return "redirect:join_finish";
 		}
 		
@@ -40,26 +48,29 @@ public String join(@ModelAttribute MemberDto memberDto) {
 	
 	@GetMapping("/join_finish")
 	public String joinFinish(){
-		return "member/join_?error";
+		return "member/join_finish";
 	}	
+
 	
+	
+	// 로그인 화면 보여준후, 
 	@GetMapping("/login")
 	public String login() {
 		
 		return "member/login";
 	}
-	
+	// 로그인 ID, PW 입력시 성공 실패 매서드 구현 
 	@PostMapping("/login")
 	public String login( @ModelAttribute MemberDto memberDto, 
 			HttpSession session) {
 	
-// 첫번째 로그인 성공시, 실패시  메소드 
+// 첫번째 방법: 로그인 성공시, 실패시  메소드 
 // result >0 크면 로그인 성공 (저장된 디비의 id,pw와 함께 로그인시 (결과화면: result=1)성공
 //		int result =sqlSession.selectOne("mnMember.siLogin1", memberDto);
 //		System.out.println("result="+result);
 //		return "redirect:/";  // localhost:8080/spring10, 즉 홈 메인화면으로 돌아옴
 
-// 두번째 	로그인 성공시, 실패시    - HomeController와  root.jsp 함께보기 
+// 두번째 방법: 로그인 성공시, 실패시 메소드  - HomeController와  root.jsp 함께보기 
 		MemberDto find = 
 				sqlSession.selectOne("mnMember.siLogin2", memberDto);
 		if(find!=null) {//성공
