fbffbdc8e74037ce796baa2835d3cf8244fd4ce3
triptocode <3580rio@gmail.com>
2022-01-24 18:30:39 +0900

    sqlSession.update("mnBoard.updateViewUp", board_no);

--- spring10homepage_dao/src/main/java/com/kk/spring10_2/repository/BoardDao.java
+++ spring10homepage_dao/src/main/java/com/kk/spring10_2/repository/BoardDao.java
@@ -6,8 +6,8 @@ public interface BoardDao {
 	//글 등록 기능
 	// - 필요데이터 : BoardDto
 	// - 결과데이터 : 게시글 번호(int)
-	int write(BoardDto boardDto);
-
-	BoardDto view(int board_no);
-	BoardDto viewUp(int board_no);
+	
+	int write(BoardDto boardDto); // 글 작성 메서드 
+	BoardDto view(int board_no);   // 상세보기 + 조회수증가 x
+	BoardDto viewUp(int board_no); // 상세보기 + 조회수증가 o
 }
