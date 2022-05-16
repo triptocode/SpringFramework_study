96277c92be760ce74f6c89cd623935b29324e0c6
triptocode <3580rio@gmail.com>
2022-05-16 23:52:53 +0900

    form action = "도착할url" name= 변수명, post 는 network-Payload에서 확인, sources에서 주석변화

--- spring05checkbox/src/main/java/com/kk/spring05/controller/CheckboxController.java
+++ spring05checkbox/src/main/java/com/kk/spring05/controller/CheckboxController.java
@@ -21,8 +21,13 @@ public class CheckboxController {
 	
 	@PostMapping("/fruit2")
 	public String threefruit2(
-			//@RequestParam String[] fruit) {
-			 @RequestParam List <String> fruit) {   //jsp에 input name ="fruit"를 변수로 사용한다 
+		// request get parametor로 못받는다
+		// 그래서 jsp, servlet에서는 request get parametor values로
+   
+			
+  //@RequestParam String[] fruit) {
+  @RequestParam List <String> fruit) {   //jsp�뿉 input name ="fruit"瑜� 蹂��닔濡� �궗�슜�븳�떎 
+  // fruit는 form의 name 값 	
 		for(String f: fruit) {
 			System.out.println("f="+f);
 		}
--- spring05checkbox/src/main/webapp/WEB-INF/views/fruit.jsp
+++ spring05checkbox/src/main/webapp/WEB-INF/views/fruit.jsp
@@ -7,12 +7,12 @@
 <h1> 구매 가능한 과일  </h1>
 
 <form action="fruit2" method="post">
-
+<%-- form action ="도착할 (컨트롤러) url " --%>
+<!--form name ="(컨트롤러) fruit 변수에 담게 된다 " -->
 	<input type="checkbox" name="fruit" value="apple"> 사과
 	<input type="checkbox" name="fruit" value="kiwi"> 키위
-	
-	<input type="checkbox" name="fruit" value="포도"> 포도
-	<input type="checkbox" name="fruit" value="체리"> 체리
+	<input type="checkbox" name="fruit" value="grape"> 포도
+	<input type="checkbox" name="fruit" value="cherry"> 체리
 	
 	
 	<br><br>
