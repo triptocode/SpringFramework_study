0ae68e48fac661e85dbc732b3e62236cd1bc4cbd
triptocode <3580rio@gmail.com>
2022-11-11 12:30:39 +0900

    회원가입, 로그인 - password encoder, 게시판 - 검색창, 조회수, 상세보기 구현

--- spring10homepage_dao/src/main/java/com/kk/spring10_2/controller/MemberController.java
+++ spring10homepage_dao/src/main/java/com/kk/spring10_2/controller/MemberController.java
@@ -1,5 +1,4 @@
 package com.kk.spring10_2.controller;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,45 +13,31 @@ import com.kk.spring10_2.repository.MemberDao;
 
 @Controller
 @RequestMapping("/member")
-public class MemberController {
-	
+public class MemberController {	
 	@Autowired
 	private MemberDao memberDao;
-
-	
+	// 회원가입 페이지 진입
 	@GetMapping("/join")
 	public String join(){	
 		return "member/join";
-	}	
-	
+	}
+	// 회원가입페이지의 <form>에서 데이터를 input 받아서 들어올때 post 처리
 	@PostMapping("/join")
-public String join(@ModelAttribute MemberDto memberDto) {
+	public String join(@ModelAttribute MemberDto memberDto) {
 		boolean result = memberDao.join(memberDto);
-		if(result) {
-			return "redirect:join_finish";
-		}
-		else {
-			return "redirect:join?error";
-		}
-	}
-	
-	
+		if(result) {return "redirect:join_finish"; }
+		else {return "redirect:join?error";}
+	}	
+	// 회원가입후 완료 페이지
 	@GetMapping("/join_finish")
-	public String joinFinish(){
-		return "member/join_finish";
-}	
-	
+	public String joinFinish(){return "member/join_finish";}	
+		
 	// 로그인 화면 보여준후, 
-	@GetMapping("/login")
-	public String login() {
-		return "member/login";
-	}
-	
-	@PostMapping("/login")
-	public String login(
-			@ModelAttribute MemberDto memberDto,
-			HttpSession session) {
-        
+	@GetMapping("/loginUrl")
+	public String login() {return "member/login";}
+	// 로그인 화면에서 id, pw 데이터 입력받은후 DB에 기존 id, pw 맞는지 여부에 따른 처리 (1.성공하면 홈으로, 2.실패하면 오류처리) 
+	@PostMapping("/loginUrl")
+	public String login(@ModelAttribute MemberDto memberDto,HttpSession session) { 
 		// if(로그인 true 성공하면)
 		if(memberDao.login(memberDto)) {
 			MemberDto find = memberDao.get(memberDto.getMember_id());
@@ -60,8 +45,7 @@ public String join(@ModelAttribute MemberDto memberDto) {
 			return "redirect:/";   // 홈으로 
 		}
 		//그 외의 모든 경우는 오류로 간주
-		else {
-			return "redirect:login?error";  // 오류페이지
+		else {return "redirect:loginUrl?error";  // 오류페이지
 		}
 	}
 }
--- spring10homepage_dao/src/main/webapp/WEB-INF/views/member/login.jsp
+++ spring10homepage_dao/src/main/webapp/WEB-INF/views/member/login.jsp
@@ -3,7 +3,7 @@
     
 <h1> 로그인 페이지 </h1>
 
-<form action ="login" method="post">
+<form action ="loginUrl" method="post">
 로그인:   <input type= "text" name="member_id"> <br><br>
 패스워드: <input type ="text" name="member_pw"> <br><br>
 <input type="submit" value="로그인">
